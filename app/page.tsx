e0665f0c57b4e73cef52ab2cc0b0d51108a05b74
Cero1417p <mregaladoc15@unc.edu.pe>
2025-12-05 11:57:38 -0500

    feat(page): change TrustSection order

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <TrustSection />
         <ProductCatalog />
+        <TrustSection />
         <ExpertSection />
         <FAQ />
       </main>
