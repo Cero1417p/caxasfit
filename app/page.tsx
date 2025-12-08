210ea500c3468875c1c29b8cb7932b7279fa6d32
Cero1417p <mregaladoc15@unc.edu.pe>
2025-12-07 23:19:34 -0500

    refactor(home): reorganize main page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
-import { TrustSection } from "@/components/TrustSection";
 import { ProductCatalog } from "@/components/ProductCatalog";
 import { ExpertSection } from "@/components/ExpertSection";
+import { TrustSection } from "@/components/TrustSection";
 import { FAQ } from "@/components/FAQ";
 import { Footer } from "@/components/Footer";
 import { FloatingWhatsApp } from "@/components/FloatingWhatsApp";
+import { FloatingCart } from "@/components/FloatingCart";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main>
+      <main className="flex-1">
         <Hero />
         <ProductCatalog />
-        <TrustSection />
         <ExpertSection />
+        <TrustSection />
         <FAQ />
       </main>
       <Footer />
       <FloatingWhatsApp />
+      <FloatingCart />
     </>
   );
 }
-
